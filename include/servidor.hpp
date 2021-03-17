1adbc57a70c1021b630b1ff48a423cd170d45042
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2021-03-17 01:44:04 -0300

    add server class

---
+++ include/servidor.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "canal.hpp"
+#include "canal_texto.hpp"
+#include "canal_voz.hpp"
+
+class Servidor{
+
+    // Atributos da classe
+    private:
+        int usuarioDonoID;
+        std::string nome, descricao, codigoConvite;
+        std::vector <Canal> canais;
+        std::vector <int> participantesIDs;
+
+    // Métodos da classe
+    public:
+        Servidor();
+        Servidor(int usuarioDonoID, std::string nome);
+
+        int getUsuarioDonoID();
+
+        void setNome(std::string const nome);
+        std::string getNome() const;
+
+        void setDescricao(std::string const descricao);
+        std::string getDescricao() const;
+
+        void setCodigoConvite(std::string const nome);
+        std::string getCodigoConvite() const;
+};
---
+++ src/servidor.cpp
@@ -0,0 +1,49 @@
+#include <string>
+
+#include "../include/servidor.hpp"
+
+// Construtor padrão
+Servidor::Servidor(){
+
+}
+
+// Construtor cópia
+Servidor::Servidor(int usuarioDonoID, std::string nome){
+    this->usuarioDonoID = usuarioDonoID;
+    this->nome = nome;
+}
+
+// Retorna o ID do usuário dono do servidor
+int Servidor::getUsuarioDonoID(){
+    return usuarioDonoID;
+}
+
+// Define o nome do servidor
+void Servidor::setNome(std::string const nome){
+    this->nome = nome;
+}
+
+// Retorna o nome do servidor
+std::string Servidor::getNome() const{
+    return nome;
+}
+
+// Define a descrição do servidor
+void Servidor::setDescricao(std::string const descricao){
+    this->descricao = descricao;
+}
+
+// Retorna a descrição do servidor
+std::string Servidor::getDescricao() const{
+    return descricao;
+}
+
+// Define o código de convite
+void Servidor::setCodigoConvite(std::string const nome){
+    this->codigoConvite = codigoConvite;
+}
+
+// Retorna o código de convite
+std::string Servidor::getCodigoConvite() const{
+    return codigoConvite;
+}
